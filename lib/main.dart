a03181b16040be2477246320cfd1254c35294033
mkubilaytekin <mkubilaytekin@gmail.com>
2023-10-25 11:02:34 +0300

    Ui finished, data finished

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,37 @@ class MyContact extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        bottomNavigationBar: ContactUsBottomAppBar(
+          backgroundColor: Colors.teal.shade300,
+          textColor: Colors.white,
+          companyName: 'Kubilay Tekin',
+          email: 'tekinkubilay08@gmail.com',
+          fontSize: 16,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ContactUs(
+                logo: const AssetImage("images/interstellar.png"),
+                companyName: "Kubilay Tekin",
+                tagLine: 'Flutter Mobile Developer',
+                email: '',
+                phoneNumber: '+905462211497',
+                githubUserName: 'mkubilaytekin',
+                linkedinURL: 'https://www.linkedin.com/in/kubilaytekins/',
+                instagram: 'mkubilaytekin',
+                textFont: '20',
+                textColor: Colors.white,
+                cardColor: Colors.teal.shade700,
+                companyColor: Colors.white70,
+                taglineColor: Colors.white70,
+
+                ),
+          ),
+        ),
+      ),
     );
   }
 }
